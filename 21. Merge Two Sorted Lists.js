e311e51d0414dd84a360f1a7ca2d8cceb3cab0a4
ywCN <wangyouhao90@gmail.com>
2018-03-26 17:32:12 -0400

    add solution2

--- 21. Merge Two Sorted Lists.js
+++ 21. Merge Two Sorted Lists.js
@@ -39,6 +39,21 @@ const mergeTwoLists = (l1, l2) => {
   return dummy.next;
 };
 
+const mergeTwoListsB = (l1, l2) => {
+  if (l1 === null) {
+    return l2;
+  }
+  if (l2 === null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  }
+  l2.next = mergeTwoLists(l1, l2.next);
+  return l2;
+};
+
 const a = new ListNode(2);
 const b = new ListNode(4);
 const c = new ListNode(6);
