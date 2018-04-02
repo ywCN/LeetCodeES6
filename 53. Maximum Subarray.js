0cd904c7c4b0376cacf7ed934f9ed7795c77080c
ywCN <wangyouhao90@gmail.com>
2018-04-01 23:10:33 -0400

    add variant

--- 53. Maximum Subarray.js
+++ 53. Maximum Subarray.js
@@ -33,4 +33,40 @@ const maxSubArray = nums => {
   return max;
 };
 
+/**
+ * variant
+ * Not asking sum, but the range
+ * If A[i] < 0, current sum + A[i] >= 0, we can continue addition because
+ * the positive sum would still contribute to positiveness of the subarray.
+ * If A[i] < 0, current sum + A[i] < 0, the current subarray has to end.
+ */
+const maxSubArrayB = A => {
+  let beginTemp = 0; // save the temporary begining index
+  let begin = 0; // begining index
+  let end = 0; // ending index
+  let maxSoFar = A[0]; // max sum of previous sequence
+  let maxEndingHere = A[0]; // max sum of this group
+
+  for (let i = 1; i < A.length; i++) {
+    // i starts from 1
+    if (maxEndingHere < 0) {
+      // last A[i] is too small
+      maxEndingHere = A[i];
+      beginTemp = i; // update begin temp
+    } else {
+      maxEndingHere += A[i];
+    }
+
+    if (maxEndingHere >= maxSoFar) {
+      // update max so far
+      maxSoFar = maxEndingHere;
+      begin = beginTemp; // save index range
+      end = i;
+    }
+  }
+  return [begin, end, maxSoFar];
+};
+
+console.log(maxSubArrayB([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
 // new file: 4/1/2018
