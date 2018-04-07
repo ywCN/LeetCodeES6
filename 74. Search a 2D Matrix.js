fea6db6137fa775ad983b0a70415f6cb092dc6fa
ywCN <wangyouhao90@gmail.com>
2018-04-06 23:45:06 -0400

    solution2

--- 74. Search a 2D Matrix.js
+++ 74. Search a 2D Matrix.js
@@ -30,4 +30,31 @@ const searchMatrix = (matrix, target) => {
   return false;
 };
 
+/**
+ * Binary Search. O(log(m+n))
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+const searchMatrixB = (matrix, target) => {
+  if (!matrix || !matrix.length || !matrix[0].length) return false;
+  let start = 0;
+  let rows = matrix.length;
+  let cols = matrix[0].length;
+  let end = rows * cols - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let rowIndex = Math.floor(mid / cols);
+    let colIndex = mid % cols;
+    if (matrix[rowIndex][colIndex] < target) {
+      start = mid + 1;
+    } else if (matrix[rowIndex][colIndex] > target) {
+      end = mid - 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+
 // new file: 4/6/2018
