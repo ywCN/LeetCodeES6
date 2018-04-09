586abc96bc7371715f953ae5d72e669aeddc7eb9
ywCN <wangyouhao90@gmail.com>
2018-04-09 00:31:48 -0400

    solution2

--- 78. Subsets.js
+++ 78. Subsets.js
@@ -42,6 +42,38 @@ const subsets = nums => {
   return res;
 };
 
+/**
+ * Bottom-up. Time:O(2^n) for every element has 2 options, stay or not. Space: O(n)
+ *
+ * Initialize the list by adding an empty [];
+ * For each number in nums[].
+ * | For each result in list.
+ *  | Get a copy of result from right to left.
+ *  | Change the copy.
+ *  | Add the copy to list.
+ * Return list.
+ *
+ * E.g.:
+ * [] -> [] [1]
+ * [] [1] -> [] [1] [2] [1, 2]
+ * Steps: 1. create copy of [], add 2 to copy of [], add[2] to answer
+ *        2. create copy of [1], add 2 to copy of [1], add [1,2] to answer
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const subsetsB = nums => {
+  const res = [];
+  res.push([]);
+  for (let num of nums) {
+    for (let i = res.length - 1; i >= 0; i--) {
+      // add from right to left, init i to free the right starting location
+      res.push([...res[i], num]); // copy, edit, and add back to res
+    }
+  }
+  return res;
+};
+
 console.log(subsets([1, 2, 3]));
+console.log(subsetsB([1, 2, 3]));
 
 // new file: 4/8/2018
